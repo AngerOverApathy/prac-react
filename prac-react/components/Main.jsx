2a89d9bb6de1f4a665f04f0db8b2692600c34598
AngerOverApathy <violasian@gmail.com>
2023-07-14 08:42:47 -0700

    prac

--- prac-react/components/Main.jsx
+++ prac-react/components/Main.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Main() { 
     return ( 
-        <div>
-            <h1>My Main Content</h1>
-            <p>More content still</p>
+        <div className='main'>
+            <h1>Fun Facts About React:</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </div>
     );
 }
