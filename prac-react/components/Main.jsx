1cf9cf370b31c3784b725929ff49dfb12aefd75b
AngerOverApathy <violasian@gmail.com>
2023-07-26 20:02:22 -0700

    practice

---
+++ prac-react/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() { 
+    return ( 
+        <div>
+            <h1>My Main Content</h1>
+            <p>More content still</p>
+        </div>
+    );
+}
+
+ export default Main;
---
+++ prac-react/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <div>
+            <h1>My Portfolio</h1>
+        </div>
+    )
+}
+
+export default Navbar;
